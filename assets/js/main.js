03b8c24d5e1fb47be99507a68f7b1f63e4c7f969
Luana Mendes <luanamendessant@outlook.com>
2023-05-22 00:58:36 -0300

    funcao mensagem aceitar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,26 @@
 const sim = document.getElementById("sim");
 const nao = document.getElementById("nao");
 
+const mensagem = 'Sabia que você queria, também te amo <3';
+
 function gerarPosicao(min, max) {
     return (Math.random() * (max - min) + min) + "%";
 }
 
-function mudarPosicao(btn){
+function mudarPosicao(btn) {
     nao.style.position = 'absolute';
     nao.style.top = gerarPosicao(10, 80);
     nao.style.left = gerarPosicao(10, 80);
 }
 
 sim.onclick = function () {
-    alert("Sabia que você queria,  te amo <3");
+    document.querySelector('.resposta').textContent = mensagem;
 }
 
 nao.onclick = function () {
     mudarPosicao();
-} 
+}
 
-nao.onmouseover = function (){
+nao.onmouseover = function () {
     mudarPosicao();
 }
